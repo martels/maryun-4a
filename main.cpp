b34fe2307c94ee70732474b6bd2db7e22534ba9e
martels <martel@husky.neu.edu>
2018-11-13 15:28:37 -0500

    final w/zip

--- main.cpp
+++ main.cpp
@@ -25,16 +25,16 @@ int main()
     {
       b1.initialize(fin);
       b1.printOriginal();
-        b1.setCell(3,3,3);
-      b1.print();
-        b1.addUpdate(3, 3, 3);
-        b1.addUpdate(4,4,2);
-        b1.addUpdate(4,5,2);
-        b1.clearUpdate(4, 4);
-        b1.clearUpdate(4, 5);
-        b1.clearUpdate(3,1);
+      b1.addUpdate(3, 3, 3);
+      b1.addUpdate(4, 4, 2);
+      b1.addUpdate(4, 5, 2);
+      b1.printConflicts();
+      b1.clearUpdate(4, 4);
+      b1.clearUpdate(4, 5);
+      b1.clearUpdate(3, 1);
       b1.printConflicts();
     }
+    exit(1);
   }
   catch (indexRangeError &ex)
   {
